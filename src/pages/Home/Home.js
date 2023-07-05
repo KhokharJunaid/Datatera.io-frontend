07fccc5c99479c2f640b180887742d32dc03e7f5
Idrees850 <mi5853261@gmail.com>
2023-07-05 13:25:08 +0500

    subscription-changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -27,7 +27,7 @@ import UploadTextModal from "../../modals/UploadTextModal/UploadTextModal";
 import useWindowDimensions from "../../utiles/getWindowDimensions";
 import "./Home.css";
 import PricingModal from "../../components/PricingModal";
-import { plan1, plan2, plan3 } from "../../service/plan";
+import { plan1, plan2, plan3, plans } from "../../service/plan";
 
 const Home = () => {
   const { list, setListItems, openSideBar, setOpenSideBar } =
@@ -405,6 +405,14 @@ const Home = () => {
       });
   };
 
+  const [userPlan, setUserPlan] = useState();
+
+  const getUserPlan = async () => {
+    await api
+      .get(`/user/me`)
+      .then((res) => setUserPlan(res.data?.subscriptions));
+  };
+
   const handleMailLinkClick = (event) => {
     // event.preventDefault();
     const mailtoLink = "mailto:mi5853361@gmail.com";
@@ -419,6 +427,7 @@ const Home = () => {
 
   useEffect(() => {
     handleTotalUploads();
+    getUserPlan();
   }, []);
 
   return (
@@ -508,7 +517,11 @@ const Home = () => {
                                   >
                                     <label
                                       for="upload_csv"
-                                      className={search?.remainingUploads === 0 ? "upload_csv_btn m-1 disabled " : "upload_csv_btn m-1" }
+                                      className={
+                                        search?.remainingUploads === 0
+                                          ? "upload_csv_btn m-1 disabled "
+                                          : "upload_csv_btn m-1"
+                                      }
                                     >
                                       Upload CSV files
                                     </label>
@@ -770,9 +783,11 @@ const Home = () => {
                           <div className="Pricing_modal">
                             <div className="plan"> Your plan </div>
                             <div className="  call_component">
-                              <PricingModal plan={plan1} />
-                              <PricingModal plan={plan2} />
-                              <PricingModal plan={plan3} />
+                              {
+                                plans.map((plan) => (
+                                  <PricingModal key={plan.id} userPlan={userPlan} plan={plan} />
+                                ))
+                              }
                             </div>
                             <div className="contact_info">
                               <div className="contact">
